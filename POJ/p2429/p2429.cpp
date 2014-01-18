e202db10fe8b59cd590d25aeb78674628399bb81
hosumi <hosumi@mail.fake>
2014-01-18 21:16:35 +0900

    POJ 2429 initial

---
+++ POJ/p2429/p2429.cpp
@@ -0,0 +1,18 @@
+/**
+   GCD & LCM Inverse
+   http://poj.org/problem?id=2429
+*/
+#include <iostream>
+using namespace std;
+
+int main(void){
+    int GCD, LCM;
+    while(cin >> GCD >> LCM){
+        // cout << " input: " << GCD << ", " << LCM << endl;
+        
+        int a = 12;
+        int b = 15;
+        cout << a << " " << b << endl;
+    }
+    return 0;
+}
