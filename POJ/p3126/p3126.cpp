133ebf47db4db4fd83745fb64557e0606584bf64
hosumi <hosumi@mail.fake>
2014-01-18 21:47:36 +0900

    POJ 3126 initial

---
+++ POJ/p3126/p3126.cpp
@@ -0,0 +1,17 @@
+/**
+   Prime Path
+   http://poj.org/problem?id=3126
+*/
+#include <iostream>
+using namespace std;
+
+int main(void){
+    int N; cin >> N; // the number of test cases <= 100
+    for(int n = 0; n < N; n++){
+        int a, b; cin >> a >> b;
+        
+        int answer = 0;
+        cout << answer << endl;
+    }
+    return 0;
+}
